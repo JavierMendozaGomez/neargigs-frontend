ca8ce86c1f8b283c665e89eb996c8dd6ea2440a3
javierenriquemendozagomez@gmail.com <javierenriquemendozagomez@gmail.com>
2019-11-02 16:58:01 +0000

    Inserting date time local\

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import HomePage from './pages/homepage/homepage.component';
 import NewConcertPage from './pages/newConcert/newConcert.component';
 import Concert from './pages/concertInfo/concertInfo.component';
 import Header from './components/header/header.component';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+
+import MomentUtils from '@date-io/moment';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/newConcert' component={NewConcertPage} />
-        <Route exact path='/concert' component={Concert} />
-      </Switch>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/newConcert' component={NewConcertPage} />
+          <Route exact path='/concert' component={Concert} />
+        </Switch>
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
--- src/components/concertForm/concertForm.component.jsx
+++ src/components/concertForm/concertForm.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './concertForm.styles.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import {createConcert} from '../../redux/concertsOverview/concertsOverview.actions';
 import {connect} from 'react-redux';
+import {TimePicker} from '@material-ui/pickers/';
 import uuidv4 from 'uuid/v4';
 
-const ConcertForm = ({createConcert}) => (
-	<form
+const ConcertForm = ({createConcert}) =>  {
+	const [selectedDate, handleDateChange] = useState(new Date());
+	return (<form
 		className='container'
 		onSubmit={(e) => {
 			e.preventDefault();
@@ -21,11 +23,14 @@ const ConcertForm = ({createConcert}) => (
 		<TextField label='Title' />
 		<TextField label='Description' />
 		<TextField label='ImageURL' />
-		<Button type='submit' variant="contained" color="primary">
+		<TimePicker autoOk label="12 hours" value={selectedDate} onChange={handleDateChange} />
+
+	  <Button type='submit' variant="contained" color="primary">
                 Create Concert
 		</Button>
 	</form>
-);
+	)
+};
 
 
 export default connect(null, {createConcert})(ConcertForm);
