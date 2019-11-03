45a5ea796328e8c3692898b9469329e425498cd9
javierenriquemendozagomez@gmail.com <javierenriquemendozagomez@gmail.com>
2019-11-03 11:05:59 +0000

    routing when click an image done

--- src/components/concertPreview/concertPreview.component.jsx
+++ src/components/concertPreview/concertPreview.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './concertPreview.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const ConcertPreview = ({title, image}) => (
+const ConcertPreview = ({history, title, image, id}) => (
 	<div className='concert-preview'>
 		<p>{title}</p>
-		<img className='previewImage' src={image} alt="Italian Trulli" />
+		<img className='previewImage' src={image} alt="Italian Trulli" onClick={() => (history.push(`/concert/${id}`))}/>
 	</div>
 );
 
-export default ConcertPreview;
+export default withRouter(ConcertPreview);
